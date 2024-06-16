4714207f47cc80450042dee150f2748d0cfaa835
Jeremy <jdejoux55@gmail.com>
2024-06-16 21:22:20 +0200

    Docker

--- app/Http/Resources/CinemaResource.php
+++ app/Http/Resources/CinemaResource.php
@@ -15,7 +15,7 @@ class CinemaResource extends JsonResource
 	public function toArray(Request $request): array
 	{
 		return [
-			'id' => $this->id,
+			'id' => $this->uid,
 			'name' => $this->name
 		];
 	}
