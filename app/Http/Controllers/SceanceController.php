4c7d7d622b700ece5dfbabb456b602e74ade7abf
Jeremy <jdejoux55@gmail.com>
2024-06-29 16:03:56 +0200

    End

--- app/Http/Controllers/SceanceController.php
+++ app/Http/Controllers/SceanceController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cinema;
 use App\Models\Sceance;
+use App\Services\MovieService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class SceanceController extends Controller
 {
+
+	protected $movieService;
+
+	public function __construct(MovieService $movieService)
+	{
+		$this->movieService = $movieService;
+	}
+
+
 	public function index($cinemaUid, $roomUid)
 	{
 		$sceances = Sceance::where('room_uid', $roomUid)->get();
 		return response()->json($sceances, 200);
 	}
 
-	public function show($cinemaUid, $roomUid, $uid)
+	public function show($roomUid, $uid)
 	{
 		$sceance = Sceance::where('room_uid', $roomUid)->where('uid', $uid)->firstOrFail();
 		return response()->json($sceance, 200);
@@ -23,11 +34,14 @@ class SceanceController extends Controller
 
 	public function store(Request $request, $cinemaUid, $roomUid)
 	{
-		$request->validate([
-			'movie' => 'required|string|max:255',
-			'date' => 'required|date',
-		]);
-
+		$movie = $this->movieService->getMovie($request->movie);
+		if (isset($movie['error'])) {
+			return response()->json(['error' => 'Movie not found'], 404);
+		}
+		$cinema = Cinema::find($cinemaUid);
+		if (!$cinema) {
+			return response()->json(['error' => 'Cinema not found'], 404);
+		}
 		$sceance = new Sceance();
 		$sceance->uid = Str::uuid()->toString();
 		$sceance->room_uid = $roomUid;
@@ -42,11 +56,21 @@ class SceanceController extends Controller
 	public function update(Request $request, $cinemaUid, $roomUid, $uid)
 	{
 		$request->validate([
-			'movie' => 'sometimes|required|string|max:255',
+			'movie' => 'sometimes|required|int',
 			'date' => 'sometimes|required|date',
 		]);
 
 		$sceance = Sceance::where('room_uid', $roomUid)->where('uid', $uid)->firstOrFail();
+
+		$cinema = Cinema::find($cinemaUid);
+		if (!$cinema) {
+			return response()->json(['error' => 'Cinema not found'], 404);
+		}
+
+		$movie = $this->movieService->getMovie($request->movie);
+		if (isset($movie['error'])) {
+			return response()->json(['error' => 'Movie not found'], 404);
+		}
 
 		if ($request->has('movie')) {
 			$sceance->movie = $request->movie;
